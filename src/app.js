3fe3126ed77dc1e5a670db70d9813e62e028c26c
Tamara Temple <tamouse@gmail.com>
2016-06-29 11:43:00 -0500

    Working on styling instead of application :/

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// import jQuery from 'jquery';
+window.jQuery = require('jquery');
+window.jQueryFlexSlider = require('flexslider');
+import './stylesheets/styles.sass';
+
+const App = (function ($, flexSlider) {
+  
+})(jQuery, jQueryFlexSlider);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const AppPaths = {};
+AppPaths.basePath = __dirname;
+AppPaths.srcPath  = path.join(AppPaths.basePath, "src");
+AppPaths.binPath  = path.join(AppPaths.basePath, "bin");
+AppPaths.sassPath = path.join(AppPaths.srcPath, 'stylesheets');
+
+
+module.exports = {
+  entry: path.resolve(AppPaths.srcPath, 'app.js'),
+  output: {
+    path: AppPaths.binPath,
+    filename: 'bundle.js'
+  },
+  devtool: "inline-source-map",
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.s[ac]ss$/, exclude: /node_modeles/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap']}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "FlexSlider Example",
+      filename: "index.html",
+      template: path.resolve(AppPaths.srcPath, "index.html.ejs")
+    }),
+    new HtmlWebpackPlugin({
+      title: "FlexSlider Example Colophon",
+      filename: "colophon.html",
+      template: path.resolve(AppPaths.srcPath, "colophon.html.ejs")
+    })
+  ]
+};
